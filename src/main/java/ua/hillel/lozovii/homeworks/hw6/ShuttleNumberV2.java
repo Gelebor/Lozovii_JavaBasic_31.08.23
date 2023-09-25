36a86092296e2cc1218d2e20e7bf42e2d4548f80
Andrii Lozovii <ingibit90@gmail.com>
2023-09-25 21:43:55 +0300

    add hw6(1/2) ShuttleNumberV2 + L7

---
+++ src/main/java/ua/hillel/lozovii/homeworks/hw6/ShuttleNumberV2.java
@@ -0,0 +1,57 @@
+package ua.hillel.lozovii.homeworks.hw6;
+
+public class ShuttleNumberV2 {
+    public static void main(String[] args) {
+        int startNum = 0;
+        int shAmount = 100;
+
+        int badDigit1 = 4;
+        int badDigit2 = 9;
+
+        int flag = 0;
+        int shGoodNumCount = 0;
+        int shBadNumCount = 0;
+
+        while (shGoodNumCount < shAmount) {
+            int range = 0;
+            int tempRange = Math.abs(startNum);
+            if (startNum == 0) {
+                range = 1;
+            } else {
+                while (tempRange != 0) {
+                    tempRange /= 10;
+                    ++range;
+                }
+            }
+
+            if (range > 1) {
+                for (int j = 0; j <= (range - 1); j++) {
+                    int devI = (int) (Math.abs(startNum) / (Math.pow(10, j)));
+                    int modI = devI % 10;
+
+                    if (modI == badDigit1 || modI == badDigit2) {
+                        ++flag;
+                    }
+                }
+            } else {
+                if (Math.abs(startNum) == badDigit1 || Math.abs(startNum) == badDigit2) {
+                    ++flag;
+                }
+            }
+
+            if (flag > 0) {
+                flag = 0;
+                ++shBadNumCount;
+                ++startNum;
+                continue;
+            }
+
+            System.out.println("Shuttle number = " + startNum);
+            ++shGoodNumCount;
+            ++startNum;
+        }
+
+        System.out.println("\nCount of bad numbers: " + shBadNumCount);
+        System.out.println("Count of good numbers: " + shGoodNumCount);
+    }
+}
---
+++ src/main/java/ua/hillel/lozovii/homeworks/hw7/L7.java
@@ -0,0 +1,65 @@
+package ua.hillel.lozovii.homeworks.hw7;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class L7 {
+    public static void main(String[] args) {
+//        int[] array1 = new int[5];
+//
+//        array1[0] = 1;
+//        array1[1] = 2;
+//        array1[2] = 3;
+//        array1[3] = 4;
+//        array1[4] = 5;
+
+//        System.out.println(array1[4]);
+//        System.out.println(array1);     //HEX, address
+
+//        System.out.println(array1.length);
+
+//        for (int i = 0; i < array1.length; i++) {
+//            System.out.println(array1[i]);
+//        }
+
+//        int[] arr2 = new int[1];
+//        System.out.println(arr2[2]);    //ArrayIndexOutOfBoundsException
+
+
+//        int[] arr3 = {1, 2, 3, 4, 5};   //input values
+//        int[] arr4 = new int[]{1, 2, 3, 4, 5};
+//        int arr5[] = {1, 2, 3, 4, 5};
+
+
+//        String[] arr6 = new String[10];
+//        for (int i = 0; i < arr6.length; i++) {
+//            System.out.println(arr6[i]);            //10 0
+//        }
+
+
+//        int[] arr = new int[10];      //100-109
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = i + 100;
+//            System.out.println(arr[i]);
+//        }
+
+
+//        int[] arr = {1, 2, 3, 4, 5};
+//
+//        for (int value : arr) {
+//            System.out.println(value);
+//        }
+//        check all value in array
+
+
+//        int[] arr = {1, 2, 3, 4, 5};
+//
+//        System.out.println(Arrays.toString(arr));
+
+
+//        ===========================
+//        ===========================
+//        ===========================
+    }
+}
