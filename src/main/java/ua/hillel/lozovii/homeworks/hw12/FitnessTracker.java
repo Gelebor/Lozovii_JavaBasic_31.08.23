45632032046910695590aff99ee2c76751653ce9
Andrii Lozovii <ingibit90@gmail.com>
2023-10-19 00:52:58 +0300

    hw12

--- src/main/java/ua/hillel/lozovii/homeworks/hw12/FitnessTracker.java
+++ src/main/java/ua/hillel/lozovii/homeworks/hw12/FitnessTracker.java
@@ -1,29 +1,114 @@
 package ua.hillel.lozovii.homeworks.hw12;
 
 public class FitnessTracker {
-//    Зробіть програму для фітнес трекера, яка реєструватиме нового користувача створюючи йому обліковий запис, приймаючи наступні параметри:
+    private final String name;
+    private String surName;
+    private final int dateDay;
+    private final int dateMonth;
+    private final int dateYear;
+    private final int age;
+    private final String email;
+    private final String phoneNumber;
+    private int weight;
+    private int bloodPreasure;
+    private int stepCount;
 
-//    Незмінні (задаються тільки при створенні облікового запису, і можуть бути отримані лише за допомогою гетерів):
-//            Ім'я
-//            Дата народження (окремо день, місяць, рік)
-//            Емейл
-//            Телефон
-//    Змінювані:
-//            Прізвище
-//            Вага
-//            Тиск
-//            Кількість пройдених за день кроків
+    public FitnessTracker(String name, String surName, int dateDay, int dateMonth, int dateYear, String email, String phoneNumber, int weight, int bloodPreasure, int stepCount) {
+        this.name = name;
+        this.dateDay = dateDay;
+        this.dateMonth = dateMonth;
+        this.dateYear = dateYear;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.surName = surName;
+        this.weight = weight;
+        this.bloodPreasure = bloodPreasure;
+        this.stepCount = stepCount;
 
-//    Створювати кожного користувача необхідно через конструктор, беручи всі поля на вхід конструктора.
+        this.age = 2023 - this.dateYear;
+    }
 
-//    Додати метод printAccountInfo(), при виклику якого друкувати всі дані про користувача
+    public String getName() {
+        return name;
+    }
 
-//    Після прийому року народження - вираховувати вік користувача у внутрішню змінну age (відштовхуючись просто від 2020 року, ускладнювати тут не будемо),
-//    на яку зробити тільки геттер і виводити на екран разом з іншими полями в методі printAccountInfo();
-//    у конструкторі або геттерах/сеттерах не використовуємо виведення в консоль.
+    public int getDateDay() {
+        return dateDay;
+    }
 
-//    У main-класі створити 3-5 користувачів та роздрукувати дані кожного через виклик методу printAccountInfo();
-//    для двох користувачів змінити кілька полів та роздрукувати нові дані повторно
+    public int getDateMonth() {
+        return dateMonth;
+    }
 
+    public int getDateYear() {
+        return dateYear;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getBloodPreasure() {
+        return bloodPreasure;
+    }
+
+    public int getStepCount() {
+        return stepCount;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setBloodPreasure(int bloodPreasure) {
+        this.bloodPreasure = bloodPreasure;
+    }
+
+    public void setStepCount(int stepCount) {
+        this.stepCount = stepCount;
+    }
+
+    public String printAccountInfo() {
+        return "\nName= " + name + ", Surname= " + surName +
+                "\nBirth Day= " + dateDay + ", Birth Month= " + dateMonth + ", Birth Year= " + dateYear + ", Age= " + age +
+                "\nEmail= " + email + ", Phone Number= " + phoneNumber +
+                "\nWeight= " + weight + ", Blood Preasure= " + bloodPreasure + ", Step Count= " + stepCount;
+    }
+
+    @Override
+    public String toString() {
+        return "FitnessTracker{" +
+                "name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", dateDay=" + dateDay +
+                ", dateMonth=" + dateMonth +
+                ", dateYear=" + dateYear +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", weight=" + weight +
+                ", bloodPreasure=" + bloodPreasure +
+                ", stepCount=" + stepCount +
+                '}';
+    }
 }
---
+++ src/main/java/ua/hillel/lozovii/homeworks/hw12/mainFitness.java
@@ -0,0 +1,30 @@
+package ua.hillel.lozovii.homeworks.hw12;
+
+public class mainFitness {
+    public static void main(String[] args) {
+        FitnessTracker human1 = new FitnessTracker("Boris", "Johnson", 19, 6, 1964, "bj@abc.com", "+5968940940", 92, 120, 5400);
+        System.out.println(human1.printAccountInfo());
+        FitnessTracker human2 = new FitnessTracker("Mel", "Gibson", 3, 1, 1956, "mg@cab.com", "+396823450", 82, 130, 3400);
+        System.out.println(human2.printAccountInfo());
+        FitnessTracker human3 = new FitnessTracker("Mads", "Mikkelsen", 22, 11, 1965, "mm@bca.com", "+1968230940", 72, 135, 6500);
+        System.out.println(human3.printAccountInfo());
+        FitnessTracker human4 = new FitnessTracker("Jeffrey", "Bridges", 4, 12, 1949, "jb@xz.com", "+6968942345", 90, 121, 5600);
+        System.out.println(human4.printAccountInfo());
+        FitnessTracker human5 = new FitnessTracker("Javier", "Bardem", 1, 3, 1969, "jb@zx.com", "+8768940940", 80, 122, 6700);
+        System.out.println(human5.printAccountInfo());
+
+        System.out.println("\nChange info for Mads Mikkelsen:\n(birth date, surname, blood preasure, weight and day step count)");
+        human3.setSurName("Arkham");
+        human3.setBloodPreasure(150);
+        human3.setWeight(75);
+        human3.setStepCount(13000);
+        System.out.println(human3.printAccountInfo());
+
+        System.out.println("\nChange info for Jeffrey Bridges:\n(birth date, surname, blood preasure, weight and day step count)");
+        human4.setSurName("Wayne");
+        human4.setBloodPreasure(133);
+        human4.setWeight(80);
+        human4.setStepCount(25000);
+        System.out.println(human4.printAccountInfo());
+    }
+}
