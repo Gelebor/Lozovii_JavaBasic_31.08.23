9606e7d2f3c1b840cb677bfb273aa3883312fc6e
Andrii Lozovii <ingibit90@gmail.com>
2023-10-23 00:54:01 +0300

    hw14 (upd)

--- src/main/java/ua/hillel/lozovii/homeworks/hw14/hw/Main.java
+++ src/main/java/ua/hillel/lozovii/homeworks/hw14/hw/Main.java
@@ -2,20 +2,17 @@ package ua.hillel.lozovii.homeworks.hw14.hw;
 
 public class Main {
     public static void main(String[] args) {
-//    Зробіть інтерфейс Smartphones, який має містити методи call(), sms(), internet()
-//    Зробіть класи, які його імплементують: Androids, iPhones.
-//    Андроїди повинні також імплементувати LinuxOS, айфони повинні імплементувати iOS
-//    Створіть екземпляри кожного виду у мейн-класі
-
         IPhones iPhone = new IPhones("iPhone 15 Pro");
         Androids android = new Androids("Google Pixel 10");
 
+        System.out.println(iPhone.getName());
         iPhone.call();
         iPhone.sms();
         iPhone.internet();
         iPhone.osVersion();
 
         System.out.println(" ");
+        System.out.println(android.getName());
         android.call();
         android.sms();
         android.internet();
