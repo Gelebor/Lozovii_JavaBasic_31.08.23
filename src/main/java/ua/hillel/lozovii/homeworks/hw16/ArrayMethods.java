24cd36945fde3c4b7a3844a95f5b6a3b9f0e5945
Andrii Lozovii <ingibit90@gmail.com>
2023-11-02 16:23:53 +0200

    Maven hw + tests (upd2)

--- src/main/java/ua/hillel/lozovii/homeworks/hw16/ArrayMethods.java
+++ src/main/java/ua/hillel/lozovii/homeworks/hw16/ArrayMethods.java
@@ -1,25 +1,6 @@
 package ua.hillel.lozovii.homeworks.hw16;
 
 public class ArrayMethods {
-    //    Додайте метод, який буде рахувати середнє арифметичне елементів масиву.
-    //    Зробіть метод, який прийматиме на вхід двомірну матрицю (двовимірний масив) у та перевіряти чи є вона квадратною
-    //    В тестовому класі створіть тестову матрицю та передайте її в метод тестованого класу
-
-//    public static void main(String[] args) {
-//        int[][] arr1 = new int[][]{
-//                {1, 2, 3, 4},
-//                {1, 2, 3, 4},
-//                {1, 2, 3, 4},
-//                {1, 2, 3, 4, 1}
-//        };
-//        int[][] arr2 = new int[2][2];
-//        int[][] arr3 = null;
-//
-//        System.out.println(ArrayMethods.squareArray(arr1));
-//        System.out.println(ArrayMethods.squareArray(arr3));
-//        System.out.println(ArrayMethods.averageArray(arr1));
-//    }
-
     public static boolean squareArray(int[][] array) {
         if (array == null) {
             return false;
@@ -30,6 +11,19 @@ public class ArrayMethods {
             }
         }
         return true;
+    }
+
+    public static double averageArray(int[][] array) {
+        int countElement = 0;
+        int countAll = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int i1 = 0; i1 < array[i].length; i1++) {
+                countElement++;
+                countAll += array[i][i1];
+            }
+        }
+        return (double) countAll / countElement;
     }
 
 //    public static String averageArray(int[][] array) {
@@ -57,17 +51,4 @@ public class ArrayMethods {
 //            return "Wrong array properties";
 //        }
 //    }
-
-    public static double averageArray(int[][] array) {
-        int countElement = 0;
-        int countAll = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int i1 = 0; i1 < array[i].length; i1++) {
-                countElement++;
-                countAll += array[i][i1];
-            }
-        }
-        return (double) countAll / countElement;
-    }
 }
